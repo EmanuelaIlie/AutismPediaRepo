9e9ddf9875b0f5ac6f0b9f18a3f54da9dd7c760b
Ema <emanuela.ilie99@e-uvt.ro>
2022-03-27 16:57:27 +0300

    Added google button

--- Project/app/src/main/java/com/example/autismpedia/ui/SplashScreenFragment.kt
+++ Project/app/src/main/java/com/example/autismpedia/ui/SplashScreenFragment.kt
@@ -1,30 +1,34 @@
 package com.example.autismpedia.ui
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.autismpedia.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.autismpedia.databinding.SplashScreenFragmentBinding
 import com.example.autismpedia.viewmodels.SplashScreenViewModel
 
 class SplashScreenFragment : Fragment() {
 
 
     private lateinit var viewModel: SplashScreenViewModel
+    private lateinit var binding: SplashScreenFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.splash_screen_fragment, container, false)
+    ): View {
+        viewModel = ViewModelProvider(this)[SplashScreenViewModel::class.java]
+        binding = SplashScreenFragmentBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
+
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(SplashScreenViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+
 
 }
