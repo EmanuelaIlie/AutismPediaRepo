13714881c6d593e1e035f62d2079a7f6c9644096
Ema <emanuela.ilie99@e-uvt.ro>
2022-04-12 19:27:19 +0300

    -add map fragment

---
+++ Project/app/src/main/java/com/example/autismpedia/ui/MapFragment.kt
@@ -0,0 +1,33 @@
+package com.example.autismpedia.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.autismpedia.R
+import com.example.autismpedia.viewmodels.MapViewModel
+
+class MapFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MapFragment()
+    }
+
+    private lateinit var viewModel: MapViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.map_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MapViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ Project/app/src/main/java/com/example/autismpedia/viewmodels/MapViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.autismpedia.viewmodels
+
+import androidx.lifecycle.ViewModel
+
+class MapViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
