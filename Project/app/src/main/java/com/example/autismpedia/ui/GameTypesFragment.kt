2f03bf91dc3a19f09cd0433e1db9d932ac42edf1
Ema <emanuela.ilie99@e-uvt.ro>
2022-04-13 18:38:22 +0300

    -add buttons and navigation on GameTypesFragment

--- Project/app/src/main/java/com/example/autismpedia/ui/GameTypesFragment.kt
+++ Project/app/src/main/java/com/example/autismpedia/ui/GameTypesFragment.kt
@@ -5,29 +5,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.autismpedia.R
+import androidx.navigation.fragment.findNavController
+import com.example.autismpedia.databinding.GameTypesFragmentBinding
 import com.example.autismpedia.viewmodels.GameTypesViewModel
 
 class GameTypesFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = GameTypesFragment()
-    }
 
+    private lateinit var binding: GameTypesFragmentBinding
     private lateinit var viewModel: GameTypesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.game_types_fragment, container, false)
+    ): View {
+        viewModel = ViewModelProvider(this)[GameTypesViewModel::class.java]
+        binding = GameTypesFragmentBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        setupObserves()
+
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(GameTypesViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun setupObserves() {
+        viewModel.onNavigateToGameIdeas.observe(viewLifecycleOwner, Observer {
+            findNavController().navigate(GameTypesFragmentDirections.actionGameTypesFragmentToGameIdeasFragment())
+        })
     }
 
 }
--- Project/app/src/main/java/com/example/autismpedia/viewmodels/GameTypesViewModel.kt
+++ Project/app/src/main/java/com/example/autismpedia/viewmodels/GameTypesViewModel.kt
@@ -1,7 +1,16 @@
 package com.example.autismpedia.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.autismpedia.utils.SingleLiveEvent
 
 class GameTypesViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+
+     private val _onNavigateToGameIdeas=SingleLiveEvent<Void>()
+     val onNavigateToGameIdeas: LiveData<Void> =_onNavigateToGameIdeas
+
+    fun onNavigateToGameIdeas(){
+        _onNavigateToGameIdeas.call()
+    }
 }
