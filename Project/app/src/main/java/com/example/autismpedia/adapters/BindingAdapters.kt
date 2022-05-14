345819d292390513ad11d0c52bff0719e268c060
Ema <emanuela.ilie99@e-uvt.ro>
2022-05-14 16:21:03 +0300

    -add firebase storage dependency

--- Project/app/src/main/java/com/example/autismpedia/adapters/BindingAdapters.kt
+++ Project/app/src/main/java/com/example/autismpedia/adapters/BindingAdapters.kt
@@ -6,6 +6,7 @@ import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.example.autismpedia.R
 import com.example.autismpedia.models.Game
+import com.google.firebase.storage.FirebaseStorage
 
 @BindingAdapter("itemTitle")
 fun TextView.setDescription(item: Game?) {
@@ -21,22 +22,22 @@ fun TextView.setUsage(item: Game?) {
     }
 }
 
-//@BindingAdapter("loadImage")
-//fun loadImage(imageView: ImageView?, game: Game) {
-//    // Load image with your image loading library ,like with Glide or Picasso or any of your favourite
-//    val storage = FirebaseStorage.getInstance()
-//    val gsReference = storage.getReferenceFromUrl("gs://skirentallicenta-ef1a0.appspot.com/${game.type}/")
-//
-//    gsReference.listAll()
-//        .addOnSuccessListener { listResult ->
-//            listResult.items.forEach { item ->
-//                if(item.toString().contains(game.id)) {
-//                    val extension = item.toString().substringAfterLast(".")
-//                    val imageRef = storage.getReferenceFromUrl("gs://skirentallicenta-ef1a0.appspot.com/${game.type}/${game.id}.$extension")
-//                    imageView?.let { Glide.with(it.context).load(imageRef).into(imageView) }
-//                }
-//            }
-//        }
-//
-//}
+@BindingAdapter("loadImage")
+fun loadImage(imageView: ImageView?, game: Game) {
+    // Load image with your image loading library ,like with Glide or Picasso or any of your favourite
+    val storage = FirebaseStorage.getInstance()
+    val gsReference = storage.getReferenceFromUrl("gs://autismpedia-e7d4a.appspot.com/${game.type}/")
+
+    gsReference.listAll()
+        .addOnSuccessListener { listResult ->
+            listResult.items.forEach { item ->
+                if(item.toString().contains(game.id.toString())) {
+                    val extension = item.toString().substringAfterLast(".")
+                    val imageRef = storage.getReferenceFromUrl("gs://autismpedia-e7d4a.appspot.com/${game.type}/${game.id}.$extension")
+                    imageView?.let { Glide.with(it.context).load(imageRef).into(imageView) }
+                }
+            }
+        }
+
+}
 
