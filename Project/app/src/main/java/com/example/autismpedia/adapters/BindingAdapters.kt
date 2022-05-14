e97ec7b2aba993770609105c696344bffc1ebafb
Ema <emanuela.ilie99@e-uvt.ro>
2022-05-14 16:19:13 +0300

    -add Glide

--- Project/app/src/main/java/com/example/autismpedia/adapters/BindingAdapters.kt
+++ Project/app/src/main/java/com/example/autismpedia/adapters/BindingAdapters.kt
@@ -3,6 +3,7 @@ package com.example.autismpedia.adapters
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import com.example.autismpedia.R
 import com.example.autismpedia.models.Game
 
@@ -20,10 +21,22 @@ fun TextView.setUsage(item: Game?) {
     }
 }
 
-@BindingAdapter("itemImage")
-fun ImageView.setItemImage(item: Game?) {
-    item?.let {
-        setImageResource(R.drawable.ic_google)
-    }
-}
+//@BindingAdapter("loadImage")
+//fun loadImage(imageView: ImageView?, game: Game) {
+//    // Load image with your image loading library ,like with Glide or Picasso or any of your favourite
+//    val storage = FirebaseStorage.getInstance()
+//    val gsReference = storage.getReferenceFromUrl("gs://skirentallicenta-ef1a0.appspot.com/${game.type}/")
+//
+//    gsReference.listAll()
+//        .addOnSuccessListener { listResult ->
+//            listResult.items.forEach { item ->
+//                if(item.toString().contains(game.id)) {
+//                    val extension = item.toString().substringAfterLast(".")
+//                    val imageRef = storage.getReferenceFromUrl("gs://skirentallicenta-ef1a0.appspot.com/${game.type}/${game.id}.$extension")
+//                    imageView?.let { Glide.with(it.context).load(imageRef).into(imageView) }
+//                }
+//            }
+//        }
+//
+//}
 
--- Project/app/src/main/java/com/example/autismpedia/models/Game.kt
+++ Project/app/src/main/java/com/example/autismpedia/models/Game.kt
@@ -5,6 +5,7 @@ import com.google.firebase.firestore.DocumentId
 data class Game (
         @DocumentId
         val id: String? = "",
+        val type: String? = "",
         val title: String? = "",
         val description: String? = "",
 )
