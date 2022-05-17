0a524e98c9f543fadb60ddce19c14a0fee52d85c
Ema <emanuela.ilie99@e-uvt.ro>
2022-05-17 14:47:15 +0300

    -add image from gallery to imageView

--- Project/app/src/main/java/com/example/autismpedia/ui/StoriesFragment.kt
+++ Project/app/src/main/java/com/example/autismpedia/ui/StoriesFragment.kt
@@ -1,17 +1,23 @@
 package com.example.autismpedia.ui
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.example.autismpedia.R
 import com.example.autismpedia.databinding.FragmentStoriesBinding
 import com.example.autismpedia.databinding.GameIdeasFragmentBinding
 import com.example.autismpedia.utils.Constants
+import com.example.autismpedia.utils.Constants.REQUEST_CODE
 import com.example.autismpedia.viewmodels.GameIdeasViewModel
 import com.example.autismpedia.viewmodels.StoriesViewModel
 import com.google.firebase.storage.FirebaseStorage
@@ -34,6 +40,27 @@ class StoriesFragment : Fragment() {
 
         binding.game = args.game
 
+        openGalleryForImage()
+
         return binding.root
     }
+
+
+    private fun openGalleryForImage() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "image/*"
+        resultLauncher.launch(intent)
+    }
+
+    private val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if(result.resultCode == Activity.RESULT_OK) {
+            val data: Intent? = result.data
+            if (data?.data != null) {
+                val imageUri: Uri = data.data!!
+                binding.ivImageOne.setImageURI(imageUri)
+            }
+        }
+    }
+
 }
--- Project/app/src/main/java/com/example/autismpedia/utils/Constants.kt
+++ Project/app/src/main/java/com/example/autismpedia/utils/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
     const val FIRESTORE_DIDACTIC_COLLECTION = "Didactic"
     const val FIRESTORE_DAILY_ACTIVITIES_COLLECTION = "DailyActivities"
     const val FIRESTORE_STORAGE_IMAGES_FOLDER = "images"
+    const val REQUEST_CODE = 200
 }
