da07103324ccd7ea0acae472d0797817af334ef8
Ema <emanuela.ilie99@e-uvt.ro>
2022-06-01 21:57:56 +0300

    -logout working

--- Project/app/src/main/java/com/example/autismpedia/ui/HomeFragment.kt
+++ Project/app/src/main/java/com/example/autismpedia/ui/HomeFragment.kt
@@ -3,12 +3,14 @@ package com.example.autismpedia.ui
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.autismpedia.R
 import com.example.autismpedia.databinding.HomeFragmentBinding
 import com.example.autismpedia.viewmodels.HomeViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeFragment : Fragment() {
 
@@ -24,9 +26,18 @@ class HomeFragment : Fragment() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
         setHasOptionsMenu(true)
+        setupObservers()
 
         return binding.root
     }
+
+    private fun setupObservers() {
+        viewModel.onLogoutClicked.observe(viewLifecycleOwner, Observer {
+            FirebaseAuth.getInstance().signOut()
+            requireActivity().finish()
+        })
+    }
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.options_menu, menu)
--- Project/app/src/main/java/com/example/autismpedia/viewmodels/HomeViewModel.kt
+++ Project/app/src/main/java/com/example/autismpedia/viewmodels/HomeViewModel.kt
@@ -1,7 +1,15 @@
 package com.example.autismpedia.viewmodels
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import com.example.autismpedia.utils.SingleLiveEvent
 
 class HomeViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    private val _onLogoutClicked = SingleLiveEvent<Void>()
+    val onLogoutClicked : LiveData<Void> = _onLogoutClicked
+
+    fun onLogoutClicked() {
+        _onLogoutClicked.call()
+    }
 }
