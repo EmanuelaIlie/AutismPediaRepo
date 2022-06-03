495b9d962ad843958e6cb8682c51c5c06cc30d07
Ema <emanuela.ilie99@e-uvt.ro>
2022-06-03 13:18:41 +0300

    -make user always choose account on SIGN IN

--- Project/app/src/main/java/com/example/autismpedia/MainActivity.kt
+++ Project/app/src/main/java/com/example/autismpedia/MainActivity.kt
@@ -10,6 +10,9 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.example.autismpedia.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 
@@ -17,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var drawerLayout: DrawerLayout
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +39,12 @@ class MainActivity : AppCompatActivity() {
     private fun setupMenuClickListeners() {
         binding.navView.setNavigationItemSelectedListener { item ->
             if (item.itemId == R.id.menu_logout) {
+                val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id)) // it is generated
+                    .requestEmail()
+                    .build()
+                googleSignInClient = GoogleSignIn.getClient(this, gso)
+                googleSignInClient.signOut()
                 FirebaseAuth.getInstance().signOut()
                 val intent = Intent(this, SignInActivity::class.java)
                 startActivity(intent)
