a4dbb19d118b7bc1001ec4ec2a2e4dec20c3cef4
Ema <emanuela.ilie99@e-uvt.ro>
2022-06-18 15:36:21 +0300

    -check if requirements are met

--- Project/app/src/main/java/com/example/autismpedia/ui/AddNewGameFragment.kt
+++ Project/app/src/main/java/com/example/autismpedia/ui/AddNewGameFragment.kt
@@ -2,6 +2,7 @@ package com.example.autismpedia.ui
 
 import android.app.Activity
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
@@ -11,21 +12,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.res.ResourcesCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.autismpedia.R
 import com.example.autismpedia.databinding.FragmentAddNewGameBinding
-import com.example.autismpedia.databinding.GameIdeasFragmentBinding
 import com.example.autismpedia.enums.GameType
 import com.example.autismpedia.models.Game
-import com.example.autismpedia.utils.Prefs
 import com.example.autismpedia.utils.State
 import com.example.autismpedia.viewmodelfactories.AddNewGameViewModelFactory
-import com.example.autismpedia.viewmodelfactories.GameIdeasViewModelFactory
 import com.example.autismpedia.viewmodels.AddNewGameViewModel
-import com.example.autismpedia.viewmodels.GameIdeasViewModel
 import kotlinx.coroutines.launch
 
 class AddNewGameFragment : Fragment() {
@@ -33,6 +35,7 @@ class AddNewGameFragment : Fragment() {
     private lateinit var viewModel: AddNewGameViewModel
     private lateinit var binding: FragmentAddNewGameBinding
     private val args: AddNewGameFragmentArgs by navArgs()
+    private var isImageReady = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -57,23 +60,23 @@ class AddNewGameFragment : Fragment() {
         viewModel.onAddNewGame.observe(viewLifecycleOwner, Observer {
             when(args.gameType) {
                 GameType.DIDACTIC -> {
-                    if(!binding.etNewGameTitle.text.isNullOrEmpty() && !binding.etNewQuestionTitle.text.isNullOrEmpty()) {
+                    if(!binding.etNewGameTitle.text.isNullOrEmpty() && !binding.etNewQuestionTitle.text.isNullOrEmpty() && isImageReady) {
                         lifecycleScope.launch {
                             val game = Game(title = binding.etNewGameTitle.text.toString(), question_title = binding.etNewQuestionTitle.text.toString())
                             addGameToFirebase(game)
                         }
                     } else {
-                        Toast.makeText(requireContext(), "Please add game title", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(requireContext(), getString(R.string.meet_all_requirements), Toast.LENGTH_SHORT).show()
                     }
                 }
                 else -> {
-                    if(!binding.etNewGameTitle.text.isNullOrEmpty()) {
+                    if(!binding.etNewGameTitle.text.isNullOrEmpty() && binding.ivAddNewGameListImage.drawable != null  && isImageReady) {
                         lifecycleScope.launch {
                             val game = Game(title = binding.etNewGameTitle.text.toString())
                             addGameToFirebase(game)
                         }
                     } else {
-                        Toast.makeText(requireContext(), "Please add game title", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(requireContext(), getString(R.string.meet_all_requirements), Toast.LENGTH_SHORT).show()
                     }
                 }
             }
@@ -108,6 +111,27 @@ class AddNewGameFragment : Fragment() {
                 val fileUri: Uri = data.data!!
                 Glide.with(requireView())
                     .load(fileUri)
+                    .listener( object : RequestListener<Drawable> {
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            isImageReady = true
+                            return false
+                        }
+
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            return false
+                        }
+                    })
                     .into(binding.ivAddNewGameListImage)
             }
         }
