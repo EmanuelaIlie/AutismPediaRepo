f2161773c5cfacdeb40aea053f3744faf3e21931
Ema <emanuela.ilie99@e-uvt.ro>
2022-06-21 13:39:32 +0300

    -google Maps working

--- Project/app/src/main/java/com/example/autismpedia/viewmodels/MapViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.autismpedia.viewmodels
-
-import androidx.lifecycle.ViewModel
-import com.example.autismpedia.repositories.GameRepository
-
-class MapViewModel(repository: GameRepository) : ViewModel() {
-    // TODO: Implement the ViewModel
-}
