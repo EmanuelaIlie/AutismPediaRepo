300563f3bc6c6b7b86bfc08d0a9c8cea2e61875b
Ema <emanuela.ilie99@e-uvt.ro>
2022-06-30 09:39:51 +0300

    -add markers on map

--- Project/app/src/main/java/com/example/autismpedia/ui/GoogleMapsFragment.kt
+++ Project/app/src/main/java/com/example/autismpedia/ui/GoogleMapsFragment.kt
@@ -26,9 +26,36 @@ class GoogleMapsFragment : Fragment() {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
-        val timisoara = LatLng(45.7489, 21.2087)
-        googleMap.addMarker(MarkerOptions().position(timisoara).title("Marker in Timisoara"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(timisoara, 12.0f))
+        val cluj1 = LatLng(46.791163, 23.643068)
+        val cluj2 = LatLng(46.759358, 23.581747)
+        val bucuresti1 = LatLng(44.421449, 26.094111)
+        val bucuresti2 = LatLng(44.432378, 26.131837)
+        val bucuresti3 = LatLng(44.420619, 26.141586)
+        val bucuresti4 = LatLng(44.408438, 26.174960)
+        val bucuresti5 = LatLng(44.399440, 26.104463)
+        val timisoara1 = LatLng(45.7489, 21.2087)
+        val timisoara2 = LatLng(45.753875, 21.246672)
+        val timisoara3 = LatLng(45.748790, 21.220835)
+        val timisoara4 = LatLng(45.746808, 21.196585)
+        val timisoara5 = LatLng(45.726427, 21.222020)
+        val timisoara6 = LatLng(45.725701, 21.188043)
+
+
+        googleMap.addMarker(MarkerOptions().position(cluj1).title("Asociatia Autism Prietenie Terapie"))
+        googleMap.addMarker(MarkerOptions().position(cluj2).title("Asociatia Autism Transilvania"))
+        googleMap.addMarker(MarkerOptions().position(bucuresti1).title("Asociatia Romana de Terapii in Autism si ADHD"))
+        googleMap.addMarker(MarkerOptions().position(bucuresti2).title("Asociatia Autism Voice"))
+        googleMap.addMarker(MarkerOptions().position(bucuresti3).title("Asociația pentru Ajutorarea Copiilor cu Autism din România - Horia Moțoi"))
+        googleMap.addMarker(MarkerOptions().position(bucuresti4).title("Autism Help Center Sun House"))
+        googleMap.addMarker(MarkerOptions().position(bucuresti5).title("Centrul Autism Step by Step - Oltenitei"))
+        googleMap.addMarker(MarkerOptions().position(timisoara1).title("Asociaţia Casa Faenza-Centrul Comunitar PT. Copii Autişti"))
+        googleMap.addMarker(MarkerOptions().position(timisoara2).title("Day Center for Children with Disabilities Long Bridge"))
+        googleMap.addMarker(MarkerOptions().position(timisoara3).title("Copii si Zane"))
+        googleMap.addMarker(MarkerOptions().position(timisoara4).title("Society for Children and Parents"))
+        googleMap.addMarker(MarkerOptions().position(timisoara5).title("Children and Fairies Center"))
+        googleMap.addMarker(MarkerOptions().position(timisoara6).title("Foundation For You"))
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(timisoara1, 12.0f))
     }
 
     override fun onCreateView(
